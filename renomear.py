ea06971ff909f6852a99dc4f90344c1b3f0d5241
alvelvis <elvis.desouza99@gmail.com>
2018-11-11 12:48:16 -0200

    renomear

---
+++ renomear.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+diretorio = input('Deseja renomear os arquivos de qual pasta?\n').replace('"','')
+while not os.path.isdir(diretorio):
+	diretorio = input('\nDeseja renomear os arquivos de qual pasta?\n').replace('"','')
+
+novonome = input('\nOs arquivos receberão um nome e uma numeração. Que nome você deseja que os arquivos recebam?\n')
+while novonome.strip() == '':
+	novonome = input('\nOs arquivos receberão um nome e uma numeração. Que nome você deseja que os arquivos recebam?\n')
+
+arquivos = os.listdir(diretorio)
+
+print('\nAguarde...\n')
+
+for i,arquivo in enumerate(arquivos):
+	if os.path.isfile(diretorio + '/' + arquivo):
+		texto = open(diretorio + '/' + arquivo, 'r').read()
+		print('(' + str(i) + '/' + str(len(arquivos)) + ') ' + arquivo)
+		os.remove(diretorio + '/' + arquivo)
+		open(diretorio + '/' + novonome + '-' + str(i) + '.txt', 'w').write(texto)
